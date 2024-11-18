485795dbaf8047eb7b45ab8c8c35060157b3007c
PShashankreddy <168917797+PShashankreddy@users.noreply.github.com>
2024-11-18 18:31:50 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,23 @@
+import math
+#task1
+print("hello world")
+
+#task2
+def sum1(a,b):
+    print(a+b)
+    print("Done by shashank")
+a=10
+b=20
+sum1(a,b)
+
+
+#task3
+
+for i in range(1,6):
+    print(i*2)
+
+
+#task4
+n=int(input("enter a number"))
+print(math.sqrt(n))
+
